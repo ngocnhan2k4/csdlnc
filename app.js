edc0f8f53b0d2234d3de510900918b8357805d3e
22120209 <minhpqdp02@gmail.com>
2024-12-04 09:50:13 +0700

    thay doi cau hinh engine chi tiet hon

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const route = require("./src/routes/index.js");
-const handlebars = require("express-handlebars");
+const expressHandlebars = require('express-handlebars');
 const dotenv = require("dotenv");
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -8,24 +8,29 @@ const bodyParser = require('body-parser');
 dotenv.config();
 const app= express();
 
-
+//Cấu hình template engine
 app.use(express.static("./src/public.js"));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.engine(
-    "hbs",
-    handlebars.engine({
-        extname: ".hbs",
-    })
-);
-app.set("view engine", "hbs");
-app.set("views", "./src/views");
+app.engine('hbs', expressHandlebars.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/src/views/layouts',
+    partialsDir: __dirname + '/src/views/partials/',
+    helpers: {
+        eq: (a, b) => a === b
+    }
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/src/views');
+
 
 route(app);
 
 
 app.listen(process.env.port, () => {
-    console.log(`Server is running on port ${process.env.port}`);
+    console.log(`Server is running at http://localhost:${process.env.port}`);
 });
