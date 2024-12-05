985d405c53bd3e4d617d9c025fce8a9686b4beb5
22120209 <minhpqdp02@gmail.com>
2024-12-05 07:39:05 +0700

    them duong dan toi css va js

--- src/controllers/CT/manageFoodController.js
+++ src/controllers/CT/manageFoodController.js
@@ -1,11 +1,13 @@
-
 const controller = {};
 controller.renderManageFood = async (req, res) => {
-    res.render("manageFood", {
-      layout: "main",
-      title: "ManageFood",
-    });
+  res.render("manageFood", {
+    layout: "main",
+    title: "ManageFood",
+    customHead: `
+      <link rel="stylesheet" href="/CT/manageFood/manageFood.css">
+      <script defer src="/CT/manageFood/manageFood.js"></script>
+  `,
+  });
 };
 
 module.exports = controller;
-
