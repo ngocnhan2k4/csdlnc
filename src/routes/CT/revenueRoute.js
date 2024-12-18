4c1dd12c853ed13023ef5efd6b1517b686681dd0
namurus <thaidinhngan@gmail.com>
2024-12-18 10:16:55 +0700

    adjust revenueRoute

--- src/routes/CT/revenueRoute.js
+++ src/routes/CT/revenueRoute.js
@@ -6,6 +6,5 @@ router.get('/dish', revenueController.getDishRevenue);// get revenue by dish
 router.get('/evaluation/:type', revenueController.getAverageEvalu);// get average evaluation
 router.get('/:type', revenueController.renderRevenueByType);// get revenue by type day, month, year
 router.get('/', revenueController.renderRevenue);// get all revenue
-router.get('/dish/:id', revenueController.getDishRevenueById);// get revenue by dish id
 
 module.exports = router;
